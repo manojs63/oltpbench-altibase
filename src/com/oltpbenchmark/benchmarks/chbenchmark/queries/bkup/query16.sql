c7c87507a3f6e835e5104c2e24f4d19c2468d8d3
Manoj Sharma <TEMBO-DOMAIN\mk2sharma@tembo.cs.uwaterloo.ca>
2018-12-15 18:18:45 -0500

    initial commit

---
+++ src/com/oltpbenchmark/benchmarks/chbenchmark/queries/bkup/query16.sql
@@ -0,0 +1,14 @@
+SELECT i_name,
+       i_price,
+       count(DISTINCT (mod((s_w_id * s_i_id),10000))) AS supplier_cnt
+FROM stock,
+     item
+WHERE i_id = s_i_id
+  AND i_data NOT LIKE 'zz%'
+  AND (mod((s_w_id * s_i_id),10000) NOT IN
+    (SELECT su_suppkey
+     FROM supplier
+     WHERE su_comment LIKE '%bad%'))
+GROUP BY i_name,
+         i_price
+ORDER BY supplier_cnt DESC;
